61cfda41c6d93e1a5d0f9055624c60741b0fbb1c
Matheus <matheus.justino@compasso.com.br>
2021-06-15 20:43:17 -0300

    Criando database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { AppConfigModule } from './app-config/app-config.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, AppConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
