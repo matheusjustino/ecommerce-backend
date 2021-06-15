db92f6c6fe874ecfbc6789da96bcacf8fad6cf56
Loan Matteus <loan.matteusz@gmail.com>
2021-06-15 20:49:37 -0300

    add: user's routes without orm

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateUserDTO } from './dtos/create-user.dto';
+import { UpdateUserDTO } from './dtos/update-user.dto';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(
+    private readonly userService: UserService
+  ) { }
+
+  @Get()
+  public findUserById(
+    @Param('id') id: string
+  ) {
+    this.userService.findUserById(id);
+  }
+
+  @Get()
+  public findAllUsers() {
+    return this.userService.findAllUsers();
+  }
+
+  @Post()
+  public createUser(
+    @Body() data: CreateUserDTO
+  ) {
+    this.userService.createUser(data);
+    console.log(data);
+  }
+
+  @Put()
+  public updateUser(
+    @Param('id') id: string,
+    @Body() data: UpdateUserDTO
+  ) {
+    this.userService.updateUser(id, data);
+  }
+
+  @Delete()
+  public deleteUser(
+    @Param('id') id: string
+  ) {
+    this.userService.deleteUser(id);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDTO } from './dtos/create-user.dto';
+import { UpdateUserDTO } from './dtos/update-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private users = [];
+  constructor(){}
+
+  public findAllUsers(): any {
+    return this.users;
+  }
+
+  public createUser(data: CreateUserDTO): any {
+    this.users.push(data);
+  }
+
+  public findUserById(id: string): any {
+  }
+
+  public updateUser(id: string, data: UpdateUserDTO): any {
+  }
+
+  public deleteUser(id: string): any {
+  }
+}
