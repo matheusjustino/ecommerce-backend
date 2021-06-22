37c9adf5dfbcbf31c0d48162b147acb911d91689
Loan Matteus <loan.matteusz@gmail.com>
2021-06-22 19:47:45 -0300

    feat: Cart's swagger routes

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -17,7 +17,7 @@ import {
 	CART_SERVICE,
 	ICartService,
 } from '@shared/src/cart/cartService.interface';
-import { CartItemModel } from '@shared/src/cart/cartModel';
+import { CartItemModel, CartModel } from '@shared/src/cart/cartModel';
 
 // GUARDS
 import { AuthGuard } from '@src/auth/guards/auth.guard';
@@ -27,7 +27,9 @@ import { hasRoles } from '@src/auth/decorators/roles.decorator';
 // DECORATORS
 import { User } from '@src/common/decorators/user.decorator';
 import { UserRole } from '@src/common/enums/user-role.enum';
+import { ApiBody, ApiOkResponse, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Cart')
 @Controller('carts')
 @UseGuards(AuthGuard, RolesGuard)
 export class CartController {
@@ -38,6 +40,9 @@ export class CartController {
 
 	@Post()
 	@hasRoles(UserRole.ADMIN, UserRole.CUSTOMER)
+	@ApiBody({ type: Object })
+	@ApiOkResponse({ type: CartModel })
+	@ApiOperation({ description: 'Cria um Cart.' })
 	public async createCart(@Body() body, @Res() res) {
 		const cart = await this.cartService.createCart(body.userId);
 		return res.status(HttpStatus.OK).json(cart);
@@ -45,6 +50,10 @@ export class CartController {
 
 	@Post('add-item/:cartId')
 	@hasRoles(UserRole.ADMIN, UserRole.CUSTOMER)
+	@ApiParam({ name: 'cartId', description: 'Deve ser passado o Id do Cart' })
+	@ApiBody({ type: CartItemModel })
+	@ApiOkResponse({ type: CartModel })
+	@ApiOperation({ description: 'Adiciona um item ao Cart.' })
 	public async addItemToCart(
 		@Param('cartId') cartId: string,
 		@Body() data: CartItemModel,
@@ -56,6 +65,10 @@ export class CartController {
 
 	@Post('remove-item/:cartId')
 	@hasRoles(UserRole.ADMIN, UserRole.CUSTOMER)
+	@ApiParam({ name: 'cartId', description: 'Deve ser passado o Id do Cart' })
+	@ApiBody({ type: Object })
+	@ApiOkResponse({ type: CartModel })
+	@ApiOperation({ description: 'Remove um item ao Cart.' })
 	public async removeItemCart(
 		@Param('cartId') cartId: string,
 		@Body() data,
@@ -67,6 +80,8 @@ export class CartController {
 
 	@Get()
 	@hasRoles(UserRole.ADMIN)
+	@ApiOkResponse({ type: [CartModel] })
+	@ApiOperation({ description: 'Lista os item do Cart.' })
 	public async getCarts(@Res() res: Response) {
 		const carts = await this.cartService.getCarts();
 		return res.status(HttpStatus.OK).json(carts);
@@ -74,6 +89,8 @@ export class CartController {
 
 	@Get('user-carts')
 	@hasRoles(UserRole.ADMIN, UserRole.CUSTOMER)
+	@ApiOkResponse({ type: [CartModel] })
+	@ApiOperation({ description: 'Lista os carts de um usuário.' })
 	public async getUserCarts(@User() user, @Res() res: Response) {
 		const carts = await this.cartService.getUserCarts(user.id);
 		return res.status(HttpStatus.OK).json(carts);
@@ -81,6 +98,9 @@ export class CartController {
 
 	@Get(':cartId')
 	@hasRoles(UserRole.ADMIN, UserRole.CUSTOMER)
+	@ApiParam({ name: 'cartId', description: 'Deve passar o id de um Cart' })
+	@ApiOkResponse({ type: CartModel })
+	@ApiOperation({ description: 'Lista um cart por meio do seu Id.' })
 	public async getCartById(
 		@Param('cartId') cartId: string,
 		@Res() res: Response,
@@ -91,6 +111,9 @@ export class CartController {
 
 	@Delete('delete-cart/:cartId')
 	@hasRoles(UserRole.ADMIN)
+	@ApiParam({ name: 'cartId', description: 'Deve passar o id de um Cart' })
+	@ApiOkResponse({ type: CartModel })
+	@ApiOperation({ description: 'Deleta um cart por meio do seu Id.' })
 	public async deleteCart(
 		@Param('cartId') cartId: string,
 		@Res() res: Response,
